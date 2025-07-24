06c4e4519e0d25483fdf711e7deb1e918716387c
Afaq Ahmd <afaqahmed468@gmail.com>
2025-07-24 15:06:19 +0500

    index of

--- index-of/app.js
+++ index-of/app.js
@@ -1,25 +1,22 @@
+function runIndexof() {
+  var paragraph = document.getElementById("paragraph");
 
+  // Current text in paragraph
+  var text = paragraph.innerText;
 
+  var txt1 = document.getElementById("text-1").value.trim();
+  var txt2 = document.getElementById("text-2").value.trim();
 
+  var findtext = text.indexOf(txt1);
 
-function runIndexof(){
+  if (findtext !== -1) {
+    // Replace txt1 with txt2
+    text = text.slice(0, findtext) + txt2 + text.slice(findtext + txt1.length);
 
-    
-    var text = 'A computer is an electronic device that manipulates data and information, capable of storing, retrieving and processing it.';
-    console.log(text);
-    
-    var txt1 = document.getElementById("text-1").value;
-    var txt2 = document.getElementById("text-2").value;
-
-    var findtext = text.indexOf(txt1);
-
-    if (findtext !== -1) {
-         text = text.slice(0, findtext) + txt2 + text.slice(findtext + txt1.length);
-         console.log(text);
-    }
-    else
-    {
-
-        console.log(text);
-    }
+    // Update paragraph on the page
+    paragraph.innerText = text;
+    console.log("Replaced Text:", text);
+  } else {
+    console.log("Text to find not found.");
+  }
 }
